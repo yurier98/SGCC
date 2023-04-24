1c83fde4c270e66da999f56c4e9d7b70d08cbe6b
Yurier <yurierjesus@gmail.com>
2023-04-23 21:58:58 -0400

    arreglo de url en notification

--- apps/security/urls.py
+++ apps/security/urls.py
@@ -1,29 +1,8 @@
 from django.urls import path
-<<<<<<< HEAD
-
 from . import views
 from django.urls import path
 
 from django.contrib.auth.views import LogoutView
-
-
-
-
-
-urlpatterns = [
-
-    # path('', views.RolListView.as_view(), name='rol_list'),
-    # path('add/', views.RolCreateView.as_view(), name='rol_create'),
-    # path('update/<int:pk>/', views.RolUpdateView.as_view(), name='rol_update'),
-    # path('desactive/<int:pk>/', views.RolDesactiveView.as_view(), name='rol_delete'),
-   # path('json/', views.json, name='json'),
-
-    path('groups', views.GroupListView.as_view(), name='groups_list'),
-    # path('add/', views.RolCreateView.as_view(), name='groups_create'),
-    # path('update/<int:pk>/', views.RolUpdateView.as_view(), name='groups_update'),
-    # path('desactive/<int:pk>/', views.RolDesactiveView.as_view(), name='groups_delete'),
-=======
-from . import views
 
 urlpatterns = [
 
@@ -35,12 +14,9 @@ urlpatterns = [
     path('rols/update/<int:pk>/', views.GroupUpdateView.as_view(), name='group_update'),
     path('rols/delete/<int:pk>/', views.GroupDeleteView.as_view(), name='group_delete'),
 
-
     #################  Permission   #####################################
     path('permission', views.PermissionListView.as_view(), name='permission_list'),
     # path('permission/add', views.PermissionCreateView.as_view(), name='permission_create'),
     # path('permission/update/<int:pk>/', views.PermissionUpdateView.as_view(), name='permission_update'),
     # path('permission/delete/<int:pk>/', views.PermissionDeleteView.as_view(), name='permission_delete'),
->>>>>>> origin/main
-
 ]
