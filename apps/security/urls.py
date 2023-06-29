4fbbf953183caed155f11de3b3aa9162be30bcd9
Yurier <yurierjesus@gmail.com>
2023-06-29 19:02:40 -0400

    Update Security

--- apps/security/urls.py
+++ apps/security/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
 
     # path('json/', views.json, name='json'),
+    path('groups', views.GroupListView.as_view(), name='groups_list'),
 
     #################  Roles   #####################################
     path('rols', views.GroupListView.as_view(), name='group_list'),
@@ -11,11 +14,9 @@ urlpatterns = [
     path('rols/update/<int:pk>/', views.GroupUpdateView.as_view(), name='group_update'),
     path('rols/delete/<int:pk>/', views.GroupDeleteView.as_view(), name='group_delete'),
 
-
     #################  Permission   #####################################
     path('permission', views.PermissionListView.as_view(), name='permission_list'),
     # path('permission/add', views.PermissionCreateView.as_view(), name='permission_create'),
     # path('permission/update/<int:pk>/', views.PermissionUpdateView.as_view(), name='permission_update'),
     # path('permission/delete/<int:pk>/', views.PermissionDeleteView.as_view(), name='permission_delete'),
-
 ]
--- apps/security/views.py
+++ apps/security/views.py
@@ -1,2 +1,27 @@
-from .Views.views_permission import *
+from django.contrib.auth.models import Group
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .Forms.groups_forms import GroupsForm
+from .Mixin.mixins import ValidatePermissionRequiredMixin
+
+
+# Create your views here.
+
+
+def json(request, self=None):
+    # data = list(Permission.objects.values())
+    # data = UserProfile.toJSON(self)
+    data = []
+    perm = Permission.objects.all()
+    for i in perm:
+        data.append(i)
+        print(data)
+
+    return JsonResponse(data, safe=False)
+
+
+
 from .Views.views_groups import *
+from .Views.views_permission import *
+
+
